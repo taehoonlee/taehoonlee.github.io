03e5676de8f606ac96684831bf051455cfba8048
Taehoon Lee <me@taehoonlee.com>
2021-01-24 12:30:54 +0900

    Update the number of citations

--- assets/js/google-scholar.js
+++ assets/js/google-scholar.js
@@ -1,6 +1,6 @@
 (function(exports) {
 	exports.googleScholar = function (repo, callback) {
-		var count = 166;  /* updated on 2021-01-18 */
+		var count = 190;  /* updated on 2021-01-24 */
 		if (typeof(callback) == "function") {
 			callback(count);
 		} else {
